d7cb40f74c2875a8abee5746703a617910ec1de4
Nayio <1904460200@qq.com>
2019-11-26 14:16:26 +0800

    静态库实现与调用

---
+++ staticlib/MathFuncsLib/pch.cpp
@@ -0,0 +1,32 @@
+﻿// pch.cpp: 与预编译标头对应的源文件
+
+#include "pch.h"
+// 当使用预编译的头时，需要使用此源文件，编译才能成功。
+#include "MathFuncsLib.h"
+
+#include <stdexcept>
+
+using namespace std;
+
+namespace MathFuncs
+{
+	double MyMathFuncs::Add(double a, double b)
+	{
+		return a + b;
+	}
+
+	double MyMathFuncs::Subtract(double a, double b)
+	{
+		return a - b;
+	}
+
+	double MyMathFuncs::Multiply(double a, double b)
+	{
+		return a * b;
+	}
+
+	double MyMathFuncs::Divide(double a, double b)
+	{
+		return a / b;
+	}
+}
